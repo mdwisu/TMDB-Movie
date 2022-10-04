0921b7236b73348529e6554823c6464b115baf86
mdwisu <dwisusanto784@gmail.com>
2022-10-04 17:28:48 +0700

    beres

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const searchbar = document.getElementById('search');
 currentPage == 1
   ? (prev.style.visibility = 'hidden')
   : (prev.style.visibility = '');
-//! tombol prev diklik
+//! tombol prev diklik untuk main
 prev.onclick = () => {
   currentPage--;
   // jika current page 0 maka ga boleh
@@ -37,7 +37,7 @@ prev.onclick = () => {
     });
 };
 
-//! tombol next di klik
+//! tombol next di klik untuk utama
 next.onclick = () => {
   currentPage++;
   search_term = searchbar.value.toLowerCase();
@@ -63,7 +63,7 @@ next.onclick = () => {
     });
 };
 
-// ambil data
+// ambil data untuk utama
 fetch(
   discover +
     new URLSearchParams({
@@ -78,10 +78,76 @@ fetch(
     makeElement(data);
   });
 
-// masuk ke search filed
+// todo masuk ke search filed
 // saat search bar di ketik/keyup
+prev.onclick = () => {
+  currentPage--;
+  search_term = searchbar.value.toLowerCase();
+  // jika ga ada isinya
+  if (search_term == '') {
+    return false;
+  }
+  // jika current page 0 maka ga boleh
+  if (currentPage == 0) {
+    currentPage = 1;
+  }
+  // jika current page 1 hidden prev
+  currentPage == 1
+    ? (prev.style.visibility = 'hidden')
+    : (prev.style.visibility = '');
+  // jika search bar ga ada valuenya
+  search_term = searchbar.value.toLowerCase();
+  if (search_term == '') {
+    return false;
+  }
+  // get data
+  fetch(
+    search +
+      new URLSearchParams({
+        api_key: api_key,
+        query: search_term,
+        page: currentPage,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      makeElement(data);
+      if (currentPage != data.total_pages) {
+        next.style.visibility = '';
+      }
+    });
+};
+
+next.onclick = () => {
+  currentPage++;
+  search_term = searchbar.value.toLowerCase();
+  // jika ga ada isinya
+  if (search_term == '') {
+    return false;
+  }
+  currentPage > 1
+    ? (prev.style.visibility = '')
+    : (prev.style.visibility = 'hidden');
+
+  fetch(
+    search +
+      new URLSearchParams({
+        api_key: api_key,
+        query: search_term,
+        page: currentPage,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      makeElement(data);
+      if (currentPage == data.total_pages) {
+        next.style.visibility = 'hidden';
+      }
+    });
+};
 searchbar.addEventListener('keyup', (event) => {
   search_term = event.target.value.toLowerCase();
+  currentPage = 1;
   // menghendel jika search bar kosong
   if (search_term == '') {
     fetch(
@@ -89,7 +155,7 @@ searchbar.addEventListener('keyup', (event) => {
         new URLSearchParams({
           api_key: api_key,
           sort_by: 'popularity.desc',
-          page: 1,
+          page: currentPage,
         })
     )
       .then((res) => res.json())
@@ -102,12 +168,22 @@ searchbar.addEventListener('keyup', (event) => {
         new URLSearchParams({
           api_key: api_key,
           query: search_term,
-          page: 1,
+          page: currentPage,
         })
     )
       .then((res) => res.json())
       .then((data) => {
+        console.log(data.total_pages);
         makeElement(data);
+        if (currentPage != data.total_pages) {
+          next.style.visibility = '';
+        }
+        if (currentPage == data.total_pages) {
+          next.style.visibility = 'hidden';
+        }
+        currentPage == 1
+          ? (prev.style.visibility = 'hidden')
+          : (prev.style.visibility = '');
       });
   }
 });
